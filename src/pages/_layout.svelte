4f0199c3d6787465158ee59e2317b3cb96066279
Yusuf <akuyusuf27@gmail.com>
2020-09-06 22:51:13 +0700

    refactor: 💡 separe global styles

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -5,59 +5,7 @@
   export let segment;
 </script>
 
-<style global>
-  /* purgecss start ignore */
-  @import 'tailwindcss/base';
-
-  @import 'tailwindcss/components';
-  /* purgecss end ignore */
-
-  @import 'tailwindcss/utilities';
-
-  h1 {
-    @apply text-4xl;
-  }
-
-  h2 {
-    @apply text-3xl;
-  }
-
-  h3 {
-    @apply text-2xl;
-  }
-
-  p,
-  ul,
-  ol,
-  span {
-    @apply text-xl;
-    @apply my-5;
-  }
-
-  li {
-    @apply my-4;
-  }
-
-  img {
-    @apply w-full;
-  }
-
-  svg {
-    @apply inline-block;
-  }
-
-  p a,
-  span a {
-    box-shadow: inset 0 -0.12em 0 theme('textColor.primary');
-    -webkit-transition: box-shadow 0.2s ease-in-out, color 0.2s ease-in-out;
-    transition: box-shadow 0.2s ease-in-out, color 0.2s ease-in-out;
-  }
-
-  p a:hover,
-  span a:hover {
-    box-shadow: inset 0 -1.5em 0 theme('textColor.primary');
-    color: theme('backgroundColor.primary');
-  }
+<style global src="../styles/global.css">
 </style>
 
 <Header {segment} />
