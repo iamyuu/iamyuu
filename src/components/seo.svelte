b8db1eaf5e657288d4c666590dd4fdd4a3667c3a
Yusuf <akuyusuf27@gmail.com>
2020-09-08 23:11:23 +0700

    feat: 🎸 add meta, open graph, twitter

--- src/components/seo.svelte
+++ src/components/seo.svelte
@@ -1,8 +1,37 @@
 <script>
+  import { stores } from '@sapper/app';
+
+  const { page } = stores();
+
   const siteName = 'iamyuu.dev';
-  export let title = undefined;
+  const siteUrl = 'https://iamyuu.dev';
+
+  export let title;
+  export let keywords;
+  export let thumbnail;
+  export let description = 'Personal website by Yusuf (@iamyuu)';
 </script>
 
 <svelte:head>
   <title>{title ? `${title} - ${siteName}` : siteName}</title>
+  <meta name="description" content={description} />
+  <meta name="keywords" content={keywords} />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="{siteUrl}{$page.path}" />
+  <meta property="og:title" content={title || siteName} />
+  <meta property="og:description" content={description} />
+  {#if thumbnail}
+    <meta property="og:image" content={thumbnail} />
+  {/if}
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="{siteUrl}{$page.path}" />
+  <meta property="twitter:title" content={title || siteName} />
+  <meta property="twitter:description" content={description} />
+  {#if thumbnail}
+    <meta property="twitter:image" content={thumbnail} />
+  {/if}
 </svelte:head>
