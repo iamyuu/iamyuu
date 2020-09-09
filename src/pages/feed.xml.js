2b351e1336fcb72427b4da01c199732e9e77bc87
Yusuf <akuyusuf27@gmail.com>
2020-09-10 06:44:01 +0700

    feat: 🎸 add rss and sitemap

---
+++ src/pages/feed.xml.js
@@ -0,0 +1,39 @@
+import posts from './tulisan/_posts';
+
+const siteUrl = 'https://iamyuu.dev';
+const blogPath = 'tulisan';
+
+const feedItem = item => `
+    <item>
+      <title>${item.title}</title>
+      <description><![CDATA[${item.desc}]]></description>
+      <link>${siteUrl}/${blogPath}/${item.slug}</link>
+      <guid isPermaLink="false">${siteUrl}/${blogPath}/${item.slug}</guid>
+      <pubDate>${new Date(item.date).toUTCString()}</pubDate>
+    </item>
+`;
+
+const renderXmlRssFeed = items => `<?xml version="1.0" encoding="UTF-8" ?>
+<rss xmlns:dc="http://purl.org/dc/elements/1.1/"
+  xmlns:content="http://purl.org/rss/1.0/modules/content/"
+  xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+  <channel>
+    <title><![CDATA[iamyuu.dev]]></title>
+    <description><![CDATA[Personal website by Yusuf (@iamyuu)]]></description>
+    <link>${siteUrl}</link>
+    <atom:link href="${siteUrl}/feed.xml" rel="self" type="application/rss+xml" />
+    <generator>Sapper</generator>
+    <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+    ${items.map(feedItem).join('\n')}
+  </channel>
+</rss>`;
+
+export function get(_, res) {
+  res.writeHead(200, {
+    'Cache-Control': `max-age=0, s-max-age=${600}`, // 10 minutes
+    'Content-Type': 'application/rss+xml'
+  });
+
+  const feed = renderXmlRssFeed(posts);
+  res.end(feed);
+}
