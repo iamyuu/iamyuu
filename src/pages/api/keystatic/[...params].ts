0bb1448c4118f2f71638944305da07aa445e566a
Muhammad Yusuf <yusuf@iamyuu.dev>
2023-11-20 08:20:01 +0700

    Remove unnecessary environment variable definitions

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -7,8 +7,8 @@ export const prerender = false;
 export const all = (context: APIContext) => {
   return makeHandler({
     config,
-    secret: context?.env?.KEYSTATIC_SECRET ?? import.meta.env.KEYSTATIC_SECRET,
-    clientId: context?.env?.KEYSTATIC_GITHUB_CLIENT_ID ?? import.meta.env.KEYSTATIC_GITHUB_CLIENT_ID,
-    clientSecret: context?.env?.KEYSTATIC_GITHUB_CLIENT_SECRET ?? import.meta.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+    secret: context?.env?.KEYSTATIC_SECRET,
+    clientId: context?.env?.KEYSTATIC_GITHUB_CLIENT_ID,
+    clientSecret: context?.env?.KEYSTATIC_GITHUB_CLIENT_SECRET,
   })(context);
 }
