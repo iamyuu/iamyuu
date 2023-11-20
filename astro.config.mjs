bbd168c648ce36342a5f19ca6035d39ac72b263c
Muhammad Yusuf <yusuf@iamyuu.dev>
2023-11-20 08:09:49 +0700

    fix env undefined

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,5 +12,19 @@ export default defineConfig({
   site: SITE_URL,
   output: "hybrid",
   adapter: cloudflare(),
-  integrations: [tailwind(), sitemap(), robotsTxt(), markdoc(), react()]
+  integrations: [tailwind(), sitemap(), robotsTxt(), markdoc(), react()],
+  vite: {
+    define: {
+      "process.env.KEYSTATIC_GITHUB_CLIENT_ID": JSON.stringify(process.env.KEYSTATIC_GITHUB_CLIENT_ID),
+      "process.env.KEYSTATIC_GITHUB_CLIENT_SECRET": JSON.stringify(process.env.KEYSTATIC_GITHUB_CLIENT_SECRET),
+      "process.env.KEYSTATIC_SECRET": JSON.stringify(process.env.KEYSTATIC_SECRET),
+      "process.env.PUBLIC_KEYSTATIC_GITHUB_APP_SLUG": JSON.stringify(process.env.PUBLIC_KEYSTATIC_GITHUB_APP_SLUG),
+
+      "import.meta.env.KEYSTATIC_GITHUB_CLIENT_ID": JSON.stringify(process.env.KEYSTATIC_GITHUB_CLIENT_ID),
+      "import.meta.env.KEYSTATIC_GITHUB_CLIENT_SECRET": JSON.stringify(process.env.KEYSTATIC_GITHUB_CLIENT_SECRET),
+      "import.meta.env.KEYSTATIC_SECRET": JSON.stringify(process.env.KEYSTATIC_SECRET),
+      "import.meta.env.PUBLIC_KEYSTATIC_GITHUB_APP_SLUG": JSON.stringify(process.env.PUBLIC_KEYSTATIC_GITHUB_APP_SLUG),
+    }
+  }
 });
+
--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -1,11 +1,14 @@
+import type { APIContext } from 'astro';
 import { makeHandler } from '@keystatic/astro/api';
 import config from "../../../../keystatic.config";
 
 export const prerender = false;
 
-export const all = makeHandler({
-  config,
-  secret: import.meta.env.KEYSTATIC_SECRET,
-  clientId: import.meta.env.KEYSTATIC_GITHUB_CLIENT_ID,
-  clientSecret: import.meta.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
-});
+export const all = (context: APIContext) => {
+  return makeHandler({
+    config,
+    secret: context?.env?.KEYSTATIC_SECRET ?? import.meta.env.KEYSTATIC_SECRET,
+    clientId: context?.env?.KEYSTATIC_GITHUB_CLIENT_ID ?? import.meta.env.KEYSTATIC_GITHUB_CLIENT_ID,
+    clientSecret: context?.env?.KEYSTATIC_GITHUB_CLIENT_SECRET ?? import.meta.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+  })(context);
+}
